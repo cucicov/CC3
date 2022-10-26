07b3091d0cc8023ec76529887c7cc120eed43f20
Dorin Cucicov <cucicov@gmail.com>
2022-10-26 15:38:27 +0300

    barebones hand tracking

--- python_hand_tracking/hand.py
+++ python_hand_tracking/hand.py
@@ -46,14 +46,3 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
-    parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")
-    args = parser.parse_args()
-    
-    client = udp_client.SimpleUDPClient(args.ip, args.port)
-
-    for x in range(10):
-        client.send_message("/filter", random.random())
-        time.sleep(1)
